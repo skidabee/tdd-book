f4434868696f8fb0298df94a3a04923b92312413
semianiuk <kidabee15@gmail.com>
2021-02-09 13:04:13 +0100

    chapter 14

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -56,7 +56,20 @@ public class TestClass {
     public void testReduceMoney() {
         Bank bank = new Bank();
         Money result = bank.reduce(Money.dollar(1), "USD");
-        assertEquals(result, Money.dollar(7));
+        assertEquals(result, Money.dollar(1));
         assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
     }
 }
